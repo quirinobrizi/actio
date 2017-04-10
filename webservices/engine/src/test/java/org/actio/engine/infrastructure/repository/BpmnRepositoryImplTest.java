90ab4c078a21f7291c31e8643ee091b9860f517a
Quirino Brizi <quirino.brizi@gmail.com>
2017-04-10 07:50:55 +0100

    Start create test

--- webservices/engine/src/test/java/org/actio/engine/infrastructure/repository/BpmnRepositoryImplTest.java
+++ webservices/engine/src/test/java/org/actio/engine/infrastructure/repository/BpmnRepositoryImplTest.java
@@ -1,5 +1,6 @@
 package org.actio.engine.infrastructure.repository;
 
+import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.when;
 
 import java.util.Arrays;
@@ -7,6 +8,8 @@ import java.util.List;
 
 import org.activiti.engine.RepositoryService;
 import org.activiti.engine.RuntimeService;
+import org.activiti.engine.repository.Model;
+import org.activiti.engine.repository.ModelQuery;
 import org.activiti.engine.repository.ProcessDefinition;
 import org.activiti.engine.repository.ProcessDefinitionQuery;
 import org.junit.Test;
@@ -29,11 +32,15 @@ public class BpmnRepositoryImplTest {
     @Mock
     private ProcessDefinitionQuery processDefinitionQuery;
     @Mock
+    private ModelQuery modelQuery;
+    @Mock
     private ProcessDefinition processDefinition1;
     @Mock
     private ProcessDefinition processDefinition2;
     @Mock
     private ProcessDefinition processDefinition3;
+    @Mock
+    private Model model;
 
     private String key1 = "key1";
     private String key2 = "key2";
@@ -45,12 +52,28 @@ public class BpmnRepositoryImplTest {
         when(processDefinitionQuery.asc()).thenReturn(processDefinitionQuery);
         List<ProcessDefinition> processDefinitions = Arrays.asList(processDefinition1, processDefinition2, processDefinition3);
         when(processDefinitionQuery.list()).thenReturn(processDefinitions);
+        when(repositoryService.createModelQuery()).thenReturn(modelQuery);
+        when(modelQuery.modelKey(anyString())).thenReturn(modelQuery);
+        when(modelQuery.singleResult()).thenReturn(model);
+        when(model.hasEditorSource()).thenReturn(true);
+        when(repositoryService.getModelEditorSource(anyString())).thenReturn(new byte[] {});
+        when(model.hasEditorSourceExtra()).thenReturn(true);
+        when(repositoryService.getModelEditorSourceExtra(anyString())).thenReturn(new byte[] {});
+
+        when(processDefinition1.getId()).thenReturn("1");
         when(processDefinition1.getKey()).thenReturn(key1);
         when(processDefinition1.getName()).thenReturn("name");
+        when(processDefinition1.getVersion()).thenReturn(1);
+
+        when(processDefinition2.getId()).thenReturn("2");
         when(processDefinition2.getKey()).thenReturn(key1);
         when(processDefinition2.getName()).thenReturn("name");
+        when(processDefinition2.getVersion()).thenReturn(2);
+
+        when(processDefinition3.getId()).thenReturn("3");
         when(processDefinition3.getKey()).thenReturn(key2);
         when(processDefinition3.getName()).thenReturn("name");
+        when(processDefinition3.getVersion()).thenReturn(1);
     }
 
 }
